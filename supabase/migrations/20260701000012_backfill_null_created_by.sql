a230b85ae817f42de17194cf526cc2a56a4ad2f7
Ubuntu <ubuntu@localhost.localdomain>
2026-07-01 05:03:26 +0800

    [GLM-CP] data: backfill NULL created_by from assigned_to

---
+++ supabase/migrations/20260701000012_backfill_null_created_by.sql
@@ -0,0 +1,5 @@
+-- Backfill NULL created_by: use assigned_to (current owner) as creator fallback
+BEGIN;
+UPDATE leads SET created_by = assigned_to WHERE created_by IS NULL AND assigned_to IS NOT NULL;
+NOTIFY pgrst, 'reload schema';
+COMMIT;

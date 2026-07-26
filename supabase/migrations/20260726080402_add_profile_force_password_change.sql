f48306ed70f63d68d8604e7a658798fd289a3daf
69755354 <69755354@qq.com>
2026-07-26 16:07:21 +0800

    chore(db): remove superseded migration version

--- supabase/migrations/20260726080402_add_profile_force_password_change.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE public.profiles
-  ADD COLUMN IF NOT EXISTS force_password_change BOOLEAN NOT NULL DEFAULT false;
-
-COMMENT ON COLUMN public.profiles.force_password_change IS
-  'Requires the user to change their password before normal application use.';
-
-NOTIFY pgrst, 'reload schema';
